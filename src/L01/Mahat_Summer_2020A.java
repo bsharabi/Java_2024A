2e883acb74a4fc6f799116c014a5f626475de2e0
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2023-12-27 11:12:53 +0200

    L01 Mahat summer 2020 A

---
+++ src/L01/Mahat_Summer_2020A.java
@@ -0,0 +1,107 @@
+package L01;
+
+import java.util.Scanner;
+
+public class Mahat_Summer_2020A {
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        while (true) {
+            System.out.println("Enter q number");
+            int num = sc.nextInt();
+            System.out.println("------------------------------------------");
+            switch (num) {
+                case 1:
+                    q1();
+                    break;
+                case 2:
+                    Clock c = new Clock(20, 45);
+                    System.out.println(c.getInterval());
+
+                    Clock c1 = new Clock(24, 0);
+                    System.out.println(c1.getInterval());
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 10:
+                    break;
+                case -1:
+                    return;
+
+            }
+
+        }
+    }
+
+    private static void q1() {
+        int cntNum = 0, cntEv = 0, cntPos = 0, sumPos = 0;
+        int num;
+
+//        num = sc.nextInt();
+//        while (num != 0) {
+//            cntNum++;
+//            cntEv += (num % 2 == 0) ? 1 : 0;
+//            if (num > 0) {
+//                sumPos += num;
+//                cntPos++;
+//            }
+//            num = sc.nextInt();
+//        }
+
+        while ((num = sc.nextInt()) != 0) {
+            cntNum++;
+            cntEv += (num % 2 == 0) ? 1 : 0;
+            if (num > 0) {
+                sumPos += num;
+                cntPos++;
+            }
+        }
+//        double avg = (cntPos == 0) ? 0 : (double) sumPos / cntPos;
+        double avg = (double) sumPos / cntPos;
+        System.out.println(ANSI_BLUE + "counter  -> " + cntNum);
+        System.out.println(ANSI_GREEN + "Even -> " + cntEv);
+        System.out.print(ANSI_RED);
+        System.out.println("Pos -> " + cntPos);
+        System.out.println(ANSI_CYAN + "Sum Pos -> " + sumPos);
+        System.out.println(ANSI_YELLOW + "Avg -> " + avg);
+        System.out.println(ANSI_RESET);
+    }
+
+    static class Clock {
+        private int hour; //0-23 שעה
+        private int min; //0-59 דקות
+
+        public Clock(int hour, int min) {
+            this.hour = hour;
+            this.min = min;
+        }
+
+        public int getInterval() {
+
+//            int dayMin = 1440;
+//            int currentDayMin = hour * 60 + min;
+//            return dayMin - currentDayMin;
+
+
+            //      (23 - 20)  * 60 +  (60 - 45)
+            //        180           +     15 = 195
+            return (23 - hour) * 60 + (60 - min);
+        }
+    }
+
+
+}
