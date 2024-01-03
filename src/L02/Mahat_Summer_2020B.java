7fcc255d847d0006500727dc1c7f839bfd588a56
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2024-01-03 09:14:14 +0200

    new mahat l02

---
+++ src/L02/Mahat_Summer_2020B.java
@@ -0,0 +1,63 @@
+package L02;
+
+import java.util.Scanner;
+
+public class Mahat_Summer_2020B {
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int ans;
+        boolean bool;
+        while (true) {
+            System.out.println("Enter q number");
+            int num = sc.nextInt();
+            System.out.println("------------------------------------------");
+            switch (num) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    break;
+                case 11:
+                    break;
+                case 12:
+                    break;
+                case 13:
+                    break;
+                case -1:
+                    return;
+
+            }
+
+        }
+    }
+
+
+}
